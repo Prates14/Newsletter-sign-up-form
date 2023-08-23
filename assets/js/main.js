57fa91cb6ae1e30a22f006ecb3083db049e21c54
Prates <guipratesdsantos@gmail.com>
2023-08-23 12:30:14 -0300

    challenger 01 frontendMentor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 class EnivoDoEmail {
     constructor() {
         this.btn = document.querySelector('button');
-        this.input = document.querySelector('#sub-email');
+        this.input = document.querySelector('.sub-email');
         this.evento();
     }
 
@@ -9,12 +9,12 @@ class EnivoDoEmail {
         this.btn.addEventListener('click', event => {
             event.preventDefault();
             const validaEmail = this.validaEmail(this.input.value);
-            
-            if(validaEmail === true) {
+
+            if (validaEmail === true) {
                 window.location.href = 'success.html';
-                const page = window.successPage(this.input);
+                localStorage.setItem('email', JSON.stringify(this.input.value));
             } else {
-                
+                this.erroDetectado();
             }
         });
     }
@@ -24,14 +24,14 @@ class EnivoDoEmail {
         const usuario = valor.substring(0, valor.indexOf('@'));
         const dominio = valor.substring(valor.indexOf("@") + 1, valor.length);
 
-        if ((usuario.length >= 1) && 
-            (dominio.length >= 3) && 
-            (usuario.search("@") == -1) && 
-            (dominio.search("@") == -1) && 
-            (usuario.search(" ") == -1) && 
-            (dominio.search(" ") == -1) && 
-            (dominio.search(".") !== -1) && 
-            (dominio.indexOf(".") >= 1) && 
+        if ((usuario.length >= 1) &&
+            (dominio.length >= 3) &&
+            (usuario.search("@") == -1) &&
+            (dominio.search("@") == -1) &&
+            (usuario.search(" ") == -1) &&
+            (dominio.search(" ") == -1) &&
+            (dominio.search(".") !== -1) &&
+            (dominio.indexOf(".") >= 1) &&
             (dominio.lastIndexOf(".") < dominio.length - 1)) {
             resultado = true;
         } else {
@@ -41,9 +41,17 @@ class EnivoDoEmail {
         return resultado;
     }
 
-    successPage(nomeEmail) {
-        const email = document.querySelector('b');
-        email.innerHTML = nomeEmail.value;
+    erroDetectado() {
+        const span = document.querySelector('span')
+        this.input.classList.add('error-email')
+        document.querySelector('.erro-msg').classList.add('error-content')
+        span.innerHTML = 'Valid email required'
     }
+
 }
 const validandoEmail = new EnivoDoEmail();
+
+function page2() {
+    const frase = JSON.parse(localStorage.getItem('email'));
+    document.querySelector('.text').innerHTML = `A confirmation email has been sent to <b>${frase}</b>. Please open it and click the button inside to confirm your subscription.`
+}
